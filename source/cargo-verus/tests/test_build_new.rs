c56d01f5d46d9739016e4003e763c886e06a8082
Mate Kovacs <mkovaxx@gmail.com>
2026-04-15 08:50:07 +0900

    add new test

--- source/cargo-verus/tests/test_build_new.rs
+++ source/cargo-verus/tests/test_build_new.rs
@@ -1,4 +1,7 @@
-use cargo_verus::{ExecutionPlan, test_utils::MockPackage};
+use cargo_verus::{
+    ExecutionPlan,
+    test_utils::{MockDep, MockPackage, MockWorkspace},
+};
 
 #[test]
 fn lib_with_example_imports_own_lib() {
@@ -45,4 +48,65 @@ fn bin_only_no_own_lib_import() {
         "driver args should not import a lib for a bin-only package: {:?}",
         driver_args,
     );
+}
+
+#[test]
+fn workspace_workdir() {
+    let optin = "optin";
+    let optout = "optout";
+    let unset = "unset";
+    let hasdeps = "hasdeps";
+
+    let workspace_dir = MockWorkspace::new()
+        .members([
+            MockPackage::new(optin).lib().verify(true),
+            MockPackage::new(optout).lib().verify(false),
+            MockPackage::new(unset).lib(),
+            MockPackage::new(hasdeps).lib().deps([MockDep::workspace(optin)]).verify(true),
+        ])
+        .materialize();
+
+    let verify_optin_prefix = format!("__VERUS_DRIVER_VERIFY_{optin}-0.1.0-");
+    let verify_optout_prefix = format!("__VERUS_DRIVER_VERIFY_{optout}-0.1.0-");
+    let verify_unset_prefix = format!("__VERUS_DRIVER_VERIFY_{unset}-0.1.0-");
+    let verify_hasdeps_prefix = format!("__VERUS_DRIVER_VERIFY_{hasdeps}-0.1.0-");
+
+    let current_dir = Some(workspace_dir.path());
+    let args = ["cargo-verus", "build", "--release", "--", "--expand-errors", "--rlimit=100"];
+    let plan = cargo_verus::plan_execution(current_dir, args).expect("plan");
+
+    let ExecutionPlan::RunCargo(cargo_plan) = plan else {
+        panic!("expected `ExecutionPlan::RunCargo`");
+    };
+
+    assert_eq!(cargo_plan.subcommand, "build");
+    assert_eq!(cargo_plan.args, ["--release"]);
+
+    let driver_args = cargo_plan.parse_driver_args(" __VERUS_DRIVER_ARGS__");
+    assert!(
+        !driver_args.contains(&"--expand-errors"),
+        "forwarded Verus args should not be in __VERUS_DRIVER_ARGS__"
+    );
+    assert!(
+        !driver_args.contains(&"--rlimit=100"),
+        "forwarded Verus args should not be in __VERUS_DRIVER_ARGS__"
+    );
+
+    let optin_driver_args = cargo_plan
+        .parse_driver_args_for_key_prefix(&format!(" __VERUS_DRIVER_ARGS_FOR_{optin}-0.1.0-"));
+    assert!(optin_driver_args.contains(&"--expand-errors"));
+    assert!(optin_driver_args.contains(&"--rlimit=100"));
+
+    let hasdeps_driver_args = cargo_plan
+        .parse_driver_args_for_key_prefix(&format!(" __VERUS_DRIVER_ARGS_FOR_{hasdeps}-0.1.0-"));
+    assert!(hasdeps_driver_args.contains(&"--expand-errors"));
+    assert!(hasdeps_driver_args.contains(&"--rlimit=100"));
+
+    cargo_plan.assert_env_has("RUSTC_WRAPPER");
+    cargo_plan.assert_env_sets("__CARGO_DEFAULT_LIB_METADATA", "verus");
+    cargo_plan.assert_env_sets("__VERUS_DRIVER_VIA_CARGO__", "1");
+    cargo_plan.assert_env_sets_key_prefix(&verify_optin_prefix, "1");
+    cargo_plan.assert_env_sets_key_prefix(&verify_hasdeps_prefix, "1");
+    cargo_plan.assert_env_has_no_key_prefix(&verify_optout_prefix);
+    cargo_plan.assert_env_has_no_key_prefix(&verify_unset_prefix);
 }
