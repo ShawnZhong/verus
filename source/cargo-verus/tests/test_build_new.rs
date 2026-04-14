ddd5ca1c2b37660dd3f91af6343501a4cfe6e78e
Mate Kovacs <mkovaxx@gmail.com>
2026-04-15 08:43:52 +0900

    add new test

--- source/cargo-verus/tests/test_build_new.rs
+++ source/cargo-verus/tests/test_build_new.rs
@@ -23,3 +23,26 @@ fn lib_with_example_imports_own_lib() {
         driver_args,
     );
 }
+
+#[test]
+fn bin_only_no_own_lib_import() {
+    let package_name = "mybin";
+    let args_prefix = format!(" __VERUS_DRIVER_ARGS_FOR_{package_name}-0.1.0-");
+
+    let project_dir = MockPackage::new(package_name).bin("main").verify(true).materialize();
+
+    let current_dir = Some(project_dir.path());
+    let args = ["cargo-verus", "build"];
+    let plan = cargo_verus::plan_execution(current_dir, args).expect("plan");
+
+    let ExecutionPlan::RunCargo(cargo_plan) = plan else {
+        panic!("expected `ExecutionPlan::RunCargo`");
+    };
+
+    let driver_args = cargo_plan.parse_driver_args_for_key_prefix(&args_prefix);
+    assert!(
+        !driver_args.contains(&"import-dep-if-present=mybin"),
+        "driver args should not import a lib for a bin-only package: {:?}",
+        driver_args,
+    );
+}
