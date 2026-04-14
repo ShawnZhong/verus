e94acacd8a823ba0df08b21fb973e2f181e3b209
Mate Kovacs <mkovaxx@gmail.com>
2026-04-15 01:18:32 +0900

    nit

--- source/cargo-verus/src/main.rs
+++ source/cargo-verus/src/main.rs
@@ -15,7 +15,7 @@ use anyhow::Result;
 use cargo_verus::{execute_plan, plan_execution};
 
 fn main() -> Result<ExitCode> {
-    let args = Vec::<String>::from_iter(env::args());
+    let args: Vec<String> = env::args().collect();
     let plan = plan_execution(None, args.iter().map(String::as_str))?;
     let exit_code = execute_plan(&plan)?;
     Ok(exit_code)
